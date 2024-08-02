85ad6a3e3e6dd361bd3e46a76e76ee659a40c2d6
YingweiGuo <guoyingwei6@outlook.com>
2024-08-02 18:20:26 +0800

    引入session_state

--- pages/01Breed_identification.py
+++ pages/01Breed_identification.py
@@ -41,29 +41,36 @@ def page_frame():
     st.write('This tool is designed to help you identify different breeds of cattle.')
     st.write('Please upload a genotype file to begin analysis.')
     
-    # 文件上传，读取文件为df
+    # 文件上传
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
+        # 每次上传文件，总是重置gt_array
         try:
             gt_df = pd.read_csv(uploaded_file, sep=' ', header=None)
-            gt_array = gt_df.to_numpy()
-            st.info('Genotype file have been uploaded.')
+            st.session_state.gt_array = gt_df.to_numpy()
+            st.session_state.uploaded_file_name = uploaded_file.name
+            # 重置分析结果，确保重新分析
+            if 'result' in st.session_state:
+                del st.session_state['result']
+            st.info('Genotype file has been uploaded and ready for analysis.')
         except:
             st.error('Invalid file. Please upload a valid genotype file.')
     else:
-        st.info('Upload you genotype file to begin analysis.')
+        st.info('Upload your genotype file to begin analysis.')
     
     # 点击按钮，调用品种分类函数，显示结果
-    # 按钮总是显示，并且检查它是否被点击
     if st.button('Analyze'):
         # 只有当按钮被点击时，才调用品种分类函数
-        result = breed_classifier(gt_array)
+        st.session_state.result = breed_classifier(st.session_state.gt_array)
         st.subheader('Analysis Results')
-        st.write(result)
+        st.write(st.session_state.result)
     else:
-        # 可以在这里添加一些默认显示，或者仅保持显示按钮
-        st.write("Click the button above to perform breed assignment.")
-
+        # 显示默认文本或上次的分析结果
+        if 'result' in st.session_state:
+            st.subheader('Analysis Results')
+            st.write(st.session_state.result)
+        else:
+            st.write("Click the button above to perform breed assignment.")
 
 
 # 主应用逻辑，调用开始分析的函数
